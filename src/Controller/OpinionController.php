e2fb61f478693d97b90cc830eed93426ac73d9b0
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-12-11 17:37:26 +0100

    symfonty init

---
+++ src/Controller/OpinionController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Attribute\Route;
+
+class OpinionController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Home Page</body></html>'
+        );
+    }
+}
